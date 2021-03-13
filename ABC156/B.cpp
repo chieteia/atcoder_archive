5179099fa6b24f35492cda3805227b798cd27cfb
ntoshihi <ntoshihi@student.42tokyo.jp>
2021-03-14 01:13:01 +0900

    ABC156

---
+++ ABC156/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep2(i, a, b) for (int i = (a); i <= (b); ++i)
+#define all(a) (a).begin(), (a).end()
+#define def(A) A = (A > 0) ? A : -(A);
+#define sum(A) accumulate(all(A), 0);
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+const ll INF = 1LL << 60;
+
+int ri() {
+	int n;
+	cin >> n;
+	return n;
+}
+
+int main()
+{
+	int n = ri();
+	int k = ri();
+	int ans = 1;
+
+	while (n)
+	{
+		if (n / k == 0)
+			cout << ans << endl;
+		ans++;
+		n /= k;
+	}
+}
