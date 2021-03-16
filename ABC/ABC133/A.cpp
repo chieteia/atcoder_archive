a5960f2daf439f2095e96ccb52f0d6db077ec9de
ntoshihi <toshi31n@icloud.com>
2021-03-16 14:46:15 +0900

    1st submission

--- ABC/ABC133/A.cpp
+++ ABC/ABC133/A.cpp
@@ -17,5 +17,9 @@ inline string rs() {string s;cin >> s;return s;}
 
 int main()
 {
+	int n, a, b;
+	cin >> n >> a >> b;
 
+	if (n * a > b) cout << b << endl;
+	else cout << n * a << endl;
 }
--- ABC/ABC133/B.cpp
+++ ABC/ABC133/B.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 #define rep(i, n) for (int i = 0; i < (n); i++)
-#define rep2(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rep2(i, a, b) for (int i = (a); i < (b); ++i)
 #define fore(i,a) for(auto &i:a)
 #define all(a) (a).begin(), (a).end()
 #define def(A) A = (A > 0) ? A : -(A);
@@ -17,5 +17,28 @@ inline string rs() {string s;cin >> s;return s;}
 
 int main()
 {
+	int n = ri();
+	int d = ri();
+	vector<vector<int>> x(n, vector<int>(d));
+	rep(i, n) rep(j, d)
+		x[i][j] = ri();
 
+	int ans = 0;
+	rep(i, n - 1){
+		int diff = 0;
+		rep2(j, i + 1, n){
+			int square = 0;
+			rep(k, d){
+				diff = x[i][k] - x[j][k];
+				square += diff * diff;
+			}
+			for (int n = 1; n * n <= square; n++){
+				if (n * n == square){
+					ans++;
+					break;
+				}
+			}
+		}
+	}
+	cout << ans << endl;
 }
